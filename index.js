5ade089e1ef18592082a5165df0a70b769ce621b
Dawn Madden <coachdawnmadden@gmail.com>
2023-08-17 20:13:31 -0400

    creates myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+const employee = {
+    name:"key",
+    streetAddress:"value",
+};
+function updateEmployeeWithKeyAndValue(employee, name, streetAddress) {
+    const newEmployee = {...employee};
+    newEmployee[name]=streetAddress
+    return newEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, name, streetAddress) {
+    employee[name] = streetAddress;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, name, streetAddress) {
+    const deleteEmploy ={...employee};
+    delete deleteEmploy[name];
+    return deleteEmploy;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, name, streetAddress) {
+    delete employee.name;
+    return employee;
+}
